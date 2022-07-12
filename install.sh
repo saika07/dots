be804b3d36cde77642eeb3c52b8f4a561a563e70
saika07 <101228523+saika07@users.noreply.github.com>
2022-07-13 00:49:25 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo echo ""
 
-mkdir -p ~/Pictures
-mkdir -p ~/Pictures/papes
-
 rmdir ~/.config/kitty
 mv -r kitty ~/.config/kitty
-mv i3/config .config/i3/config
+rm ~/.config/i3/config
+mv i3/config ~/.config/i3/config
+rm ~/.config/i3/i3blocks.conf
+mv i3/i3blocks.conf ~/.config/i3/i3blocks.conf
