466c17a1624fb549dc3166cdb5792cb61d830927
saika07 <101228523+saika07@users.noreply.github.com>
2022-07-13 00:59:17 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 sudo echo ""
 
 rmdir ~/.config/kitty
-mv -r kitty ~/.config/kitty
+cp kitty ~/.config/kitty
 rm ~/.config/i3/config
-mv i3/config ~/.config/i3/config
+cp i3/config ~/.config/i3
 rm ~/.config/i3/i3blocks.conf
-mv i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+cp i3/i3blocks.conf ~/.config/i3
